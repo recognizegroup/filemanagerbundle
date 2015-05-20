85195dc5356bdf287da5826bf8d85cd0f2fe144f
Kevin te Raa <k.teraa@recognize.nl>
2015-05-20 17:10:28 +0200

    Finished adding unit tests for the FilemanagerEventHandler

--- Tests/Resources/FilemanagerEventHandlerTest.js
+++ Tests/Resources/FilemanagerEventHandlerTest.js
@@ -29,4 +29,50 @@ describe('FilemanagerEventHandler', function() {
         expect( eventhandler.hasEvent("event") ).toEqual( true );
     });
 
+    it('should be able to register multiple listeners for the same event', function () {
+        initializeEventhandler();
+
+        eventhandler.register("event", function(){});
+        eventhandler.register("event", function(){});
+        expect( eventhandler._events.event.length ).toEqual( 2 );
+    });
+
+    it('should trigger a listener when an event is triggered', function () {
+        initializeEventhandler();
+        var triggered = false;
+        eventhandler.register("event", function(){
+            triggered = true;
+        });
+
+        eventhandler.trigger("event");
+        expect( triggered ).toEqual( true );
+    });
+
+    it('should trigger every listener when an event is triggered', function () {
+        initializeEventhandler();
+        var triggered = false;
+        var triggered2 = false;
+        eventhandler.register("event", function(){
+            triggered = true;
+        });
+
+        eventhandler.register("event", function(){
+            triggered2 = true;
+        });
+
+        eventhandler.trigger("event");
+        expect( triggered && triggered2 ).toEqual( true );
+    });
+
+    it('should be able to clear all listeners for the same event', function () {
+        initializeEventhandler();
+
+        eventhandler.register("event", function(){});
+        eventhandler.register("event", function(){});
+        eventhandler.unRegister("event");
+        expect( eventhandler._events.event ).toEqual( undefined );
+    });
+
+
+
 });
